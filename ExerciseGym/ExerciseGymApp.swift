b10f03f51ed67543968882588169bf7f5aa99420
VolodymyrDziu <credo-planers-0i@icloud.com>
2025-03-07 16:09:40 +0200

    Initial Commit

---
+++ ExerciseGym/ExerciseGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseGymApp.swift
+//  ExerciseGym
+//
+//  Created by Volodymyr Dziubenko on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
