e8567c6cbd7be1801d37a1042bbc786919d6b4fb
VolodymyrDziu <credo-planers-0i@icloud.com>
2025-03-07 20:31:31 +0200

    change

--- ExerciseGym/ContentView.swift
+++ ExerciseGym/ContentView.swift
@@ -7,10 +7,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+         
+            Text("My project Exercise")
         }
         .padding()
     }
